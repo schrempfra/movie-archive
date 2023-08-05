b81654fc0e8876ba243ec4b2a68194a18a802019
Raphael Schrempf <schrempfra@gmail.com>
2023-08-05 20:59:54 +0200

    update css

--- resources/views/layouts/standard/footer.blade.php
+++ resources/views/layouts/standard/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="mt-32 bg-gray-900 sm:mt-56" aria-labelledby="footer-heading">
+<footer class="mt-32 bg-gray-900" aria-labelledby="footer-heading">
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="mx-auto container px-6 py-16 sm:py-24 lg:px-8 lg:py-32">
         <div class="xl:grid xl:grid-cols-3 xl:gap-8">
