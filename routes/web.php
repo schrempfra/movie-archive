484e7673f05ef003a535f56eba0ba026dce6bf6d
Raphael Schrempf <schrempfra@gmail.com>
2023-08-05 21:08:52 +0200

    add movies route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::resource('/movies', MovieController::class)->only('show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
