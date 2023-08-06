f0ae7027d7e320373ce4b1aa3b06190e6199e1db
Raphael Schrempf <schrempfra@gmail.com>
2023-08-06 17:22:40 +0200

    fix

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -41,7 +41,7 @@
                         </div>
                     </div>
 
-                    @if(array_key_exists('backdrops', $movie['images']) && count($movie['images']['backdrops']) > 1))
+                    @if(array_key_exists('backdrops', $movie['images']) && count($movie['images']['backdrops']) > 1)
                         <div
                             class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
                             <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
