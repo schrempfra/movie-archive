0fcbf22b967095a4c759a4fffef6d9232a9d821d
Raphael Schrempf <schrempfra@gmail.com>
2023-08-06 15:59:20 +0200

    update heading

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,15 @@
 <x-standard-layout>
     <div class="container-full py-6 px-4 sm:px-6 lg:px-8 pt-16">
         <div class="trending-movies pb-24">
-            <h2 class="text-2xl font-bold mb-4">Trending Movies</h2>
-            <div role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 sm:gap-x-6 lg:grid-cols-8 xl:gap-x-8">
+            <h2 class="flex items-center text-2xl font-bold mb-4">
+                Trending Movies
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 ml-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+            </h2>
+            <div role="list"
+                class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 sm:gap-x-6 lg:grid-cols-8 xl:gap-x-8">
                 @foreach ($trendingMovies as $movie)
                     <x-movie-card :movie="$movie" />
                 @endforeach
@@ -10,8 +17,15 @@
         </div>
 
         <div class="popular-movies pb-24">
-            <h2 class="text-2xl font-bold">Popular Movies</h2>
-            <div role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 sm:gap-x-6 lg:grid-cols-8 xl:gap-x-8">
+            <h2 class="flex items-center text-2xl font-bold mb-4">
+                Popular Movies
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 ml-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+            </h2>
+            <div role="list"
+                class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 sm:gap-x-6 lg:grid-cols-8 xl:gap-x-8">
                 @foreach ($popularMovies as $movie)
                     <x-movie-card :movie="$movie" />
                 @endforeach
@@ -19,8 +33,15 @@
         </div>
 
         <div class="top-rated-movies pb-24">
-            <h2 class="text-2xl font-bold">Top Rated</h2>
-            <div role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 sm:gap-x-6 lg:grid-cols-8 xl:gap-x-8">
+            <h2 class="flex items-center text-2xl font-bold mb-4">
+                Top Rated
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 ml-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+            </h2>
+            <div role="list"
+                class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 sm:gap-x-6 lg:grid-cols-8 xl:gap-x-8">
                 @foreach ($topRated as $movie)
                     <x-movie-card :movie="$movie" />
                 @endforeach
